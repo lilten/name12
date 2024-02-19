736e87cdca97d2172970e31de88cad3b34395415
lilten <125046502+lilten@users.noreply.github.com>
2024-02-19 12:03:16 +0600

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,45 @@
+class Turtle(object):
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
+    
+    def go_up(self):
+        self.y += self.s
+        return self.y
+        
+    def go_down(self):
+         self.y -= self.s
+         return self.y
+
+    def go_left(self):
+        self.x -= self.s
+        return self.x
+
+    def go_right(self):
+        self.x += self.s
+        return self.x
+
+    def evolve(self):
+        self.s += 1
+        return self.s
+    
+    def degrade(self):
+        if self.s <= 0:
+            print("error")
+            return
+        self.s -= 1
+        return self.s
+
+
+    def count_moves(self, x2, y2):
+        return self.x - x2, self.y - y2
+    
+r = Turtle(10, 10, 1)
+print(r.go_up())
+print(r.go_down())
+print(r.go_left())
+print(r.go_right())
+print(r.evolve())
+print(r.degrade())
+print(r.count_moves(10, 15))
